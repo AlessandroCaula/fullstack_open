b57ddb7c676d27586c9286229762e61acbd3af20
AlessandroCaula <alecaula96@gmail.com>
2025-03-17 23:57:33 +0100

    small change

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -91,7 +91,7 @@ function App() {
   const [countriesCollection, setCountriesCollection] = useState(null)
   const [countryToDisplay, setCountryToDisplay] = useState(null)
 
-  // Weather API key
+  // Weather API key - https://www.weatherapi.com/my/
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   // Fetching the countries data from the api
