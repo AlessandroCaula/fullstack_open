be76e3d6fbb29afced1d851840b26828915ae6b3
Alessandro Caula <alecolin96@gmail.com>
2025-03-17 18:11:57 +0100

    Small implementation

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -9,7 +9,7 @@ const SearchCountry = ({ handleSearchChange, countryFilter }) => {
       <input
         placeholder="Search country"
         onChange={handleSearchChange}
-      // value={countryFilter}
+        value={countryFilter}
       />
     </div>
   )
@@ -18,14 +18,54 @@ const SearchCountry = ({ handleSearchChange, countryFilter }) => {
 // Countries List component
 const CountryList = ({ filteredCountries }) => {
 
+  // If the filteredCountries is null, return.
   if (!filteredCountries)
     return
 
-  return (
-    <div>
-      Country List
-    </div>
-  )
+  // console.log(filteredCountries)
+
+  // If the filtered Countries is higher than 10. Show that there are too many countries to visualize
+  if (filteredCountries.length > 10) {
+    return (
+      <div>
+        <div>Too many matches, specify another filter</div>
+      </div>
+    )
+  } else {
+    if (filteredCountries.length == 1) {
+      // If there is only one matched country display it. 
+      const displayCountry = filteredCountries[0]
+      const languages = ["a", "b", "c"]
+      console.log(displayCountry)
+      return (
+        <div>
+          <h1>{displayCountry.name.common}</h1>
+          <div>
+            <div>Capital: {displayCountry.capital}</div>
+            <div>Area: {displayCountry.area}</div>
+          </div>
+          <div>
+            <h2>Languages</h2>
+            {/* <ul>
+              {displayCountry.languages.map((language, index) => (
+                <li key={index}>{language}</li>
+              ))}
+            </ul> */}
+          </div>
+        </div>
+      )
+    } else {
+      // If there are more than one, and less (or equal) than 10, display only the country names. 
+      return (
+        <div>
+          {/* Map through all the countries and display their name */}
+          {filteredCountries.map((country, index) => (
+            <div key={index}>{country.name.common}</div>
+          ))}
+        </div>
+      )
+    }
+  }
 }
 
 function App() {
@@ -52,14 +92,19 @@ function App() {
     setCountryFilter(event.target.value)
   }
 
-  // Filtering out the countries based on the research
-  let filteredCountries = null
-  if (countriesCollection && countryFilter != '') {
-    filteredCountries = countriesCollection.filter(country => 
+  // Filtering out the countries based on the research. Only if the countries have been fetched.
+  const filteredCountries = countriesCollection && countryFilter != '' ?
+    countriesCollection.filter(country =>
       country.name.common.toLowerCase().includes(countryFilter.toLowerCase())
-    )
-    console.log(filteredCountries)
-  }
+    ) : null
+
+  // let filteredCountries = null
+  // if (countriesCollection && countryFilter != '') {
+  //   filteredCountries = countriesCollection.filter(country => 
+  //     country.name.common.toLowerCase().includes(countryFilter.toLowerCase())
+  //   )
+  //   console.log(filteredCountries)
+  // }
 
   return (
     <div>
