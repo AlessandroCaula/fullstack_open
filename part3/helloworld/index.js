d11ff3a8937bb65b794ca197758e78999d7b621c
AlessandroCaula <alecaula96@gmail.com>
2025-03-22 00:21:54 +0100

    Backend Theory and small new implementations

--- part3/helloworld/index.js
+++ part3/helloworld/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: "1",
@@ -25,9 +27,9 @@ app.get("/", (request, response) => {
 });
 
 // Route to handle the GET request for all the notes.
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
 
 // Route to handle the GET request and retrieve a specific note by ID
 app.get("/api/notes/:id", (request, response) => {
@@ -47,6 +49,26 @@ app.delete("/api/notes/:id", (request, response) => {
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
+});
+
+// Handling the POST request and creation of a new note to the server
+app.post("/api/notes", (request, response) => {
+  // Finding the maxId of the present notes
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id))) // Number() => Function to convert to integer
+    : 0
+
+  // Accessing the data from the body property of the request object
+  const note = request.body;
+  // Adding the Id to the new note
+  note.id = String(maxId + 1)
+
+  console.log(note); // { content: 'Postman is good in testing backend', important: true }
+
+  // Adding the new note to the notes
+  notes = notes.concat(note)
+
+  response.json(note);
 });
 
 const PORT = 3001;
