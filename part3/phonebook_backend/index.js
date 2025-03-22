0582d890cd03cd530ca6c967714197b87113af2e
AlessandroCaula <alecaula96@gmail.com>
2025-03-22 12:44:25 +0100

    Exercise 3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -36,6 +36,18 @@ app.get("/api/phonebook", (request, response) => {
   response.json(phonebook);
 });
 
+// Route to handle the GET request for the info at http://localhost:3001/info
+app.get("/info", (request, response) => {
+  const text = `Phonebook has info for ${phonebook.length} people`;
+  const date = new Date()
+  response.send(
+    `<div>
+      <p>${text}<p>
+      <p>${date}<p>
+    </div>`
+  )
+})
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`server running at port ${PORT}`);
