5a89871904a4f3f58ced7a0f9cc7bcd7bd16c315
AlessandroCaula <alecaula96@gmail.com>
2025-03-22 12:33:45 +0100

    Exercise Part 3a - 3.1

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,39 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const app = http.createServer((server, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("PhoneBook Backend");
+app.use(express.json());
+
+phonebook = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+// Route to handle the GET request fo the http://localhost:3001 showing just the text
+app.get("/", (request, response) => {
+  response.send("<h1>PhoneBook</h1>");
+});
+
+// Route to handle the GET request for all the phonebook entries http://localhost:3001/api/phonebook
+app.get("/api/phonebook", (request, response) => {
+  response.json(phonebook);
 });
 
 const PORT = 3001;
