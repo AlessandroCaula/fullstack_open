122f9c2c409c3b12c404d50e0f0397c752e6e128
AlessandroCaula <alecaula96@gmail.com>
2025-03-23 20:36:16 +0100

    Changes for running front end from the backend

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static('dist'))
 
 // Middleware that prints information about every request that is sent to the server
 const requestLogger = (request, response, next) => {
