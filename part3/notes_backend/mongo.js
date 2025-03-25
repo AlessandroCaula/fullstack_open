733cb2023a4aa711427100c82dac4851c94df3bf
AlessandroCaula <alecaula96@gmail.com>
2025-03-25 23:53:33 +0100

    Exercise 3.12

--- part3/notes_backend/mongo.js
+++ part3/notes_backend/mongo.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
+// Retrieving the string typed in the terminal. And check if there are less than 3 elements. This means that no password has been typed.
 if (process.argv.length < 3) {
   console.log("give password as argument");
   process.exit(1);
 }
 
+// Retrieve the third string typed in the terminal, which is the password for accessing to mongoDB
 const password = process.argv[2];
 
 const url = `mongodb+srv://alecaula:${password}@cluster0.ksork.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
