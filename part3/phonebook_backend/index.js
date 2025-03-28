a39b711cd601bbfaf1755f9fb284599832bcae9b
AlessandroCaula <alecaula96@gmail.com>
2025-03-28 23:27:24 +0100

    Exercises 3.13-3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,34 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
-const cors = require("cors");
+const Contacts = require("./models/phonebook");
 
-let phonebook = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const app = express();
+
+let phonebook = [];
 
 app.use(express.json());
 app.use(express.static("dist"));
-app.use(cors);
 
 // Define a custom token for logging the request body
 morgan.token("body", (req) => {
@@ -43,9 +23,11 @@ app.get("/", (request, response) => {
   response.send("<h1>PhoneBook</h1>");
 });
 
-// Route to handle the GET request for all the phonebook entries http://localhost:3001/api/phonebook
+// Route to handle the GET request for all the phonebook from MongoDB
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  Contacts.find({}).then((contact) => {
+    response.json(contact);
+  });
 });
 
 // Route to handle the GET request for the info at http://localhost:3001/info
@@ -61,42 +43,24 @@ app.get("/info", (request, response) => {
   );
 });
 
-// Route to get the information for a single phonebook entry.
+// Route to get the information for a single phonebook entry from mongoDB.
 app.get("/api/persons/:id", (request, response) => {
   // Retrieve the id of the requested person to display
   const id = request.params.id;
-  // Retrieve the person with the specified id in the phonebook
-  const person = phonebook.find((p) => p.id === id);
 
-  // Check if the person exists or not. If not, send the error message
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Contacts.findById(id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(404).end();
+    });
 });
-
-// Functionality to delete a single phonebook entry
-app.delete("/api/persons/:id", (request, response) => {
-  // Retrieve the id of the person that we want to delete from the phonebook collection.
-  const id = request.params.id;
-  // Filter out the id from the phonebook collection
-  phonebook = phonebook.filter((p) => p.id !== id);
-
-  // Sending the response back to the server
-  response.status(204).end();
-});
-
-// Function used to generate a random id
-const generateId = () => {
-  const randomId = String(Math.trunc(Math.random() * 1000));
-  // Check if the Id already exists in the phonebook
-  if (phonebook.some((p) => p.id === randomId)) {
-    // If it exists, recursively call this function to generate a new Id
-    return generateId();
-  }
-  return randomId;
-};
 
 // Implementing the POST request allowing to add new entries
 app.post("/api/persons/", (request, response) => {
@@ -117,18 +81,29 @@ app.post("/api/persons/", (request, response) => {
   }
 
   // Otherwise create a new object for the person to add to the phonebook
-  const person = {
-    id: generateId(),
+  const person = new Contacts({
     name: body.name,
     number: body.number,
-  };
+  });
 
   // Adding the new person to the phonebook
-  phonebook = phonebook.concat(person);
-  response.json(person);
+  person.save().then((savedContact) => {
+    response.json(savedContact);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+// Functionality to delete a single phonebook entry
+app.delete("/api/persons/:id", (request, response) => {
+  // Retrieve the id of the person that we want to delete from the phonebook collection.
+  const id = request.params.id;
+  // Filter out the id from the phonebook collection
+  phonebook = phonebook.filter((p) => p.id !== id);
+
+  // Sending the response back to the server
+  response.status(204).end();
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} - http://localhost:${PORT}`);
 });
