3f98ca6c7ad72d306165058301d09194079c18f0
AlessandroCaula <alecaula96@gmail.com>
2025-03-28 20:12:41 +0100

    Test branches

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -130,5 +130,5 @@ app.post("/api/persons/", (request, response) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} - - http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT} - http://localhost:${PORT}`);
 });
