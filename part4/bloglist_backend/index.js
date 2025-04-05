65fea4fcc2a08eb0ea3c620c6bd85160db9b33c7
AlessandroCaula <alecaula96@gmail.com>
2025-04-05 16:03:21 +0200

    New implementations for the Exercise 4.1. To be completed

--- part4/bloglist_backend/index.js
+++ part4/bloglist_backend/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+// Adding a middleware
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method)
+  console.log("Path:", request.path)
+  console.log("Body", request.body)
+  console.log("---")
+  next()
+}
+
+app.use(express.json());
+app.use(requestLogger)
+
 let blogs = [
   {
     id: "1",
@@ -31,6 +43,30 @@ app.get('/', (request, response) => {
 
 app.get('/api/blogs', (request, response) => {
   response.json(blogs)
+})
+
+app.post('/api/blogs/', (request, response) => {
+  // Retrieve the added blog body
+  const body = request.body
+
+  if (!body) {
+    return response.status(400).json({
+      error: "content missing"
+    })
+  }
+  
+  const blogId = String(10)
+  const blog = {
+    id: blogId,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  blogs = blogs.concat(blog)
+
+  response.json(blog)
 })
 
 const PORT = 3001
---
+++ part4/bloglist_backend/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://alecaula:${password}@cluster0.ksork.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+const blog = new Blog({
+  title: "Blog 1",
+  author: "Author 1",
+  utl: "url 1",
+  likes: 33,
+})
+
+blog.save().then((result) => {
+  console.log("blog saved!")
+  mongoose.connection.close()
+})
