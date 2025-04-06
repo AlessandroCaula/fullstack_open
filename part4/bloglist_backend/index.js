076e77b37a799de4b9d16d8de3693698bc5dc212
AlessandroCaula <alecaula96@gmail.com>
2025-04-06 22:51:31 +0200

    Completed exercise 4.1

--- part4/bloglist_backend/index.js
+++ part4/bloglist_backend/index.js
@@ -1,21 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 
-const Blog = require('./models/blog')
-
 const app = express()
 
-app.use(express.json())
+const Blog = require('./models/blog')
 
 // Adding a middleware
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 };
 
+app.use(express.json())
 app.use(requestLogger)
 
 app.get('/', (request, response) => {
