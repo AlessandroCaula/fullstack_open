90a289b991bae0fa91c18c049f605a1ce8152672
AlessandroCaula <alecaula96@gmail.com>
2025-04-09 23:37:23 +0200

    Exercise 4.4

--- part4/bloglist_backend/tests/list_helper.test.js
+++ part4/bloglist_backend/tests/list_helper.test.js
@@ -4,8 +4,35 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-    const blogs = []
-    const result = listHelper.dummy(blogs)
+  const blogs = []
+  const result = listHelper.dummy(blogs)
     
-    assert.strictEqual(result, 1)
+  assert.strictEqual(result, 1)
+})
+
+// Testing the totalLikes function
+describe('total likes', () => {
+
+  // List with one blog only
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  test('when list has only one blog, equals to the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  // Retrieve the list of blogs
+  const listOfBlogs = require('../utils/list_blogs')
+  test('when the list has multiple blogs', () => {
+    const result = listHelper.totalLikes(listOfBlogs)
+    assert.strictEqual(result, 36)
+  })
 })
--- part4/bloglist_backend/utils/list_helper.js
+++ part4/bloglist_backend/utils/list_helper.js
@@ -1,7 +1,18 @@
+// Dummy function, that will return 1 for every blog in blogs list
 const dummy = (blogs) => {
   return 1
 }
 
+// Define a function that will compute the total sum of likes in all of the blog posts
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy, 
+  totalLikes
 }
