9b9fcab5d252055b3114eaccd934638c53598ed0
AlessandroCaula <alecaula96@gmail.com>
2025-08-20 23:48:01 +0200

    Adding the post request for the calculator.

--- part9/typescript-first-project/index.ts
+++ part9/typescript-first-project/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { calculator, Operation } from "./calculator";
 const app = express();
 
+app.use(express.json());
+
 app.get("/ping", (_req, res) => {
   res.send("pong");
 });
