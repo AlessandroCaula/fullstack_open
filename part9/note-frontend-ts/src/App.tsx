c0e6b8817cea6586732a744bf8137aed0879292e
AlessandroCaula <alecaula96@gmail.com>
2025-09-11 15:49:35 +0200

    Bug fix for the Note interface

--- part9/note-frontend-ts/src/App.tsx
+++ part9/note-frontend-ts/src/App.tsx
@@ -3,26 +3,27 @@ import React, { useEffect, useState } from "react";
 
 // Let's define a type for the notes
 interface Note {
-  id: string;
+  id: number;
   content: string;
 }
 
 const App = () => {
   const [newNote, setNewNote] = useState("");
-  const [notes, setNotes] = useState<Note[]>([{ id: "1", content: "testing" }]);
+  const [notes, setNotes] = useState<Note[]>([]);
 
   // Fetching the notes from the backend
   useEffect(() => {
     axios.get<Note[]>("http://localhost:3001/notes").then((response) => {
       console.log(response.data);
-      // Converting the id from number to string
-      const noteData: Note[] = response.data.map((n) => ({
-        ...n,
-        id: String(n.id),
-      }));
+      
+      // // Converting the id from number to string
+      // const noteData: Note[] = response.data.map((n) => ({
+      //   ...n,
+      //   id: String(n.id),
+      // }));
 
       // Setting the notes
-      setNotes(noteData);
+      setNotes(response.data);
     });
   }, []);
 
@@ -30,7 +31,8 @@ const App = () => {
     event.preventDefault();
     const noteToAdd = {
       content: newNote,
-      id: String(notes.length + 1),
+      // id: String(notes.length + 1),
+      id: notes.length + 1,
     };
     setNotes(notes.concat(noteToAdd));
     setNewNote("");
