8fa369bf42bb3bbda6e8b56b6d8a2139cc5537e1
AlessandroCaula <alecaula96@gmail.com>
2025-09-22 17:40:37 +0200

    Exercise 9.24 - Extended a patient information's page in the frontend to list date, description and diagnoseCodes of the patient's entries.

--- part9/patientor-frontend/src/components/PatientInformation.tsx
+++ part9/patientor-frontend/src/components/PatientInformation.tsx
@@ -2,8 +2,8 @@ import { useParams } from "react-router-dom";
 import { Patient } from "../types";
 import { useEffect, useState } from "react";
 import patientService from "../services/patients";
-import FemaleIcon from '@mui/icons-material/Female';
-import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
 
 // interface Props {
 //   patients: Patient[];
@@ -24,7 +24,7 @@ const PatientInformation = () => {
 
       // Get the patient information
       const patient = await patientService.getPatient(id);
-      console.log(patient);
+      // console.log(patient);
       setPatient(patient);
     };
 
@@ -42,14 +42,34 @@ const PatientInformation = () => {
       <div>
         <h2>
           {patient.name}
-          {patient.gender === "male" 
-            ? <MaleIcon /> 
-            : <FemaleIcon />
-          }
+          {patient.gender === "male" ? <MaleIcon /> : <FemaleIcon />}
         </h2>
       </div>
       <p>ssn: {patient.ssn}</p>
       <p>Occupation: {patient.occupation}</p>
+      {/* If the patient has some defined entries then render them */}
+      {(patient.entries !== undefined && patient.entries?.length > 0) ? (
+        <div>
+          <h3>Entries</h3>
+          {/* Render the entries of the patients */}
+          {patient.entries?.map((entry) => (
+            <div key={entry.id}>
+              <p>
+                - {entry.date}:{" "}
+                <span style={{ fontStyle: "italic" }}>{entry.description}</span>
+              </p>
+              {/* Render the diagnoses codes */}
+              <ul>
+                {entry.diagnosisCodes?.map((code) => (
+                  <li key={code}>{code}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- part9/patientor-frontend/src/types.ts
+++ part9/patientor-frontend/src/types.ts
@@ -4,6 +4,59 @@ export interface Diagnosis {
   latin?: string;
 }
 
+// Base Entry type
+interface BaseEntry {
+  id: string,
+  description: string, 
+  date: string,
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+// Enum for Health Checking Rate
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+// Interface for sick leave
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+// Interface for Discharge
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+// Expanding the BaseEntry interface for the HealthCheckEntry type
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+// Expanding the BaseEntry interface for the OccupationalHealthcareEntry type
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+// Expanding the BaseEntry interface for the HospitalEntry type
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export enum Gender {
   Male = "male",
   Female = "female",
@@ -17,6 +70,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
